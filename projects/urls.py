d49ab7c007b71d8bdca24ac96b53f99e394ce1ec
Alexander Levero <Alexander.Levero@gmail.com>
2023-04-25 00:08:39 +0300

    Feature 5 with 1 error

---
+++ projects/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from projects.views import (
+    list_projects,
+)
+
+
+urlpatterns = [
+    path("", list_projects, name="list_projects"),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from projects.models import Project
+
 
 # Create your views here.
+def list_projects(request):
+    projects = Project.objects.all()
+    context = {
+        "list_projects": projects,
+    }
+    return render(request, "projects/list.html", context)
